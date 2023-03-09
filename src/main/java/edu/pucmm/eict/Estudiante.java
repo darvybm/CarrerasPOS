5e23c4eda16c2f9117b1653c995c95eea7ac3daa
gabrielcg01 <gcepeda@orkapi.com>
2023-03-08 23:52:44 -0400

    agregando funcionalidad formulario

---
+++ src/main/java/edu/pucmm/eict/Estudiante.java
@@ -0,0 +1,71 @@
+package edu.pucmm.eict;
+
+public class Estudiante {
+    public static int idEstudiante = 1;
+    private int id;
+    private String nombre;
+    private String sexo;
+    private int edad;
+    private String provincia;
+    private String carrera;
+
+    public Estudiante(String nombre, String sexo, int edad, String provincia, String carrera) {
+        this.id = Estudiante.idEstudiante;
+        Estudiante.idEstudiante++;
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.provincia = provincia;
+        this.carrera = carrera;
+    }
+
+    public Estudiante() {};
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -5,8 +5,29 @@ import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.JavalinRenderer;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "https://api.powerbi.com/beta/4017af08-a2e1-4daa-9180-5b3bc8ee84fa/datasets/c04cf1c6-7e64-4a30-8856-bcd7a31ae80c/rows?language=en-us&disableBranding=1&authComplete=true&key=Ttp74qaF3h6a9vlAj4qigN0nFqTLQhK4wX7YGGTb0ClLHyEPCiRdAjxk%2BrsMUge4brC%2F0xygqsdIP%2FN7uDig1w%3D%3D";
+        List<Estudiante> estudiantes = new ArrayList<>();
+
         JavalinRenderer.register(new JavalinThymeleaf(), ".html");
         Javalin app = Javalin.create().start(4000);
         app.cfg.staticFiles.add(staticFileConfig -> {
@@ -14,8 +35,57 @@ public class Main {
             staticFileConfig.directory = "/public";
             staticFileConfig.location = Location.CLASSPATH;
         });
-        app.get("/hello", ctx -> ctx.html("Hello World from Javalin!"));
         app.get("/", ctx -> ctx.redirect("/templates/home.html"));
+
+        app.post("/registrarCarrera", ctx -> {
+           String carrera = ctx.formParam("carrera");
+           String nombre = ctx.formParam("nombre");
+           String sexo = ctx.formParam("sexo");
+           String provincia = ctx.formParam("provincia");
+           int edad = ctx.formParamAsClass("edad", Integer.class).get();
+
+            Estudiante estudiante = new Estudiante(nombre,sexo,edad,provincia,carrera);
+            JSONObject json = new JSONObject();
+            json.put("id", estudiante.getId());
+            json.put("nombre", estudiante.getNombre());
+            json.put("edad", estudiante.getEdad());
+            json.put("sexo", estudiante.getSexo());
+            json.put("provincia", estudiante.getProvincia());
+//            json.put("carrera", estudiante.getCarrera());
+            json.put("carrera", "prueba (NULL)");
+
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.put(json);
+
+            String jsonString = jsonArray.toString();
+
+            enviarPowerBI(jsonString, url);
+            estudiantes.add(estudiante);
+            ctx.redirect("/");
+        });
+
+    }
+
+    private static void enviarPowerBI(String json, String urlPoweBi) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        try {
+            HttpPost httpPost = new HttpPost(urlPoweBi);
+
+            httpPost.setHeader("Content-type", "application/json");
+            System.out.println(json);
+            httpPost.setEntity(new StringEntity(json, StandardCharsets.UTF_8));
+
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            System.out.println(responseString);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            httpClient.close();
+        }
 
     }
 }
